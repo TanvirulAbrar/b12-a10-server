519480bac01bd724603486cf8abb2b0fd267c19a
TanvirulAbrar <abrartanvirul@gmail.com>
2025-11-11 17:16:53 +0600

    enroll system

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sobgiis.mongodb.net/?appName=Cluster0`;
+// const uri = `mongodb://localhost:27017`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,13 +27,53 @@ async function run() {
 
     const db = client.db("a10");
     const coursecollection = db.collection("courses");
+    const enrolledcollection = db.collection("enrolled");
+    //enrolled
+    app.get("/enroll", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
 
+      const cursor = enrolledcollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/enroll/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await enrolledcollection.findOne(query);
+      res.send(result);
+    });
+    app.post("/enroll", async (req, res) => {
+      const newp = req.body;
+      const query = { email: newp.email };
+      const data = await enrolledcollection.findOne(query);
+      if (data) {
+        console.log("hitted");
+        return res.send("already added");
+      }
+      const result = await enrolledcollection.insertOne(newp);
+      res.send(result);
+    });
+    app.patch("/enroll/:id", async (req, res) => {
+      const id = req.params.id;
+      const newp = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: newp,
+      };
+      const result = await enrolledcollection.updateOne(query, update);
+      res.send(result);
+    });
     // course
     app.get("/courses", async (req, res) => {
       const cursor = coursecollection.find().sort({ price: 1 });
       const result = await cursor.toArray();
       res.send(result);
     });
+
     app.get("/courses/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -44,6 +85,7 @@ async function run() {
       const result = await coursecollection.insertOne(newp);
       res.send(result);
     });
+
     app.delete("/courses/:id", async (req, res) => {
       const id = req.params.id;
       const queary = { _id: new ObjectId(id) };
